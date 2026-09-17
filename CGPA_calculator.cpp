fcd83dca27a040a31d5691fa91bdbc465568a98d
V3r4-4 <jonneshilary@gmail.com>
2026-09-17 19:09:38 +0300

    Initial commits

--- CGPA_calculator.cpp
+++ CGPA_calculator.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  int numCourses, grade;
+  double credits, points, total_credits, total_points, GPA, cumulative_points, cumulative_credits, quality_points;
+  cout<<"Enter the following Details:";
+  cout<<"Number of Courses: ";
+  cin>>numCourses;
+
+  cout<<"Enter your Cummulative Points: ";
+  cin>>cumulative_points;
+  cout<<"Enter Your Cumulative Credits: ";
+  cin>>cumulative_credits;
+
+  total_points=cumulative_points;
+  total_credits=cumulative_credits;
+
+  for (int i = 0; i < numCourses; i++)
+  {
+    cout<<"Enter the grade: ";
+    cin>>grade;
+    cout<<"Enter the credits: ";
+    cin>>credits;
+
+    total_points+=points;
+    points=credits 
+    total_credits+=credits;
+
+}
+  GPA=total_points/total_credits;
+  CGPA=
+  
+}
 
